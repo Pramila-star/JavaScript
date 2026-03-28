1f697f37577f82fb17cd64b031d88516adb6f372
Pramila-star <pramilaray79@gmail.com>
2026-03-28 04:41:44 +0000

    comparision injavascript

---
+++ 01_basics/01_comparision.js
@@ -0,0 +1,26 @@
+ //console.log(2 > 1);
+
+ //console.log("2" > 1);
+ //console.log("02" > 1);
+
+ console.log(null > 0);
+ console.log(null == 0);
+ console.log(null >= 0);
+
+ console.log(undefined == 0);
+ console.log(undefined > 0);
+ console.log(undefined < 0);
+
+ // ===strict check it will check the datatype and value both during comparision
+
+ console.log("2" === 2);
+ 
+
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
