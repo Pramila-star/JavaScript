fe8296e1100ef824708840b29209c7b86fa9c314
Pramila-star <pramilaray79@gmail.com>
2026-03-30 13:32:37 +0000

    about IIFE

---
+++ 03_basics/04_iife.js
@@ -0,0 +1,15 @@
+// Immediately Invoked Function     Expressions (IIFE)
+
+(function chai() {
+
+    // named IIFE
+    console.log("DB CONNECTED");
+    
+}) ();
+
+ // WE USED IIFE TO REMOVE THE POLUTION CAUSED IN GLOBAL SCOPE
+
+ ( (name) => {
+    console.log(`DB CONNECTED TWO ${name}`);
+    
+ }) ("Purnima")
