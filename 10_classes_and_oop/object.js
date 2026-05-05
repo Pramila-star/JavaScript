08f177753c4c42e99a85b68a6d8a1ca43c76c1b6
Pramila-star <pramilaray79@gmail.com>
2026-05-05 13:42:33 +0000

    new keyword uses

---
+++ 10_classes_and_oop/object.js
@@ -0,0 +1,37 @@
+function multiplyBy5(num){
+    return num*5
+}
+
+multiplyBy5.power = 2;
+
+console.log(multiplyBy5(5));
+console.log(multiplyBy5.power);
+console.log(multiplyBy5.prototype);
+
+function createUser(username, score){
+    this.username = username;
+    this.score = score;
+}
+
+createUser.prototype.increment = function(){
+    this.score++;
+}
+createUser.prototype.printMe = function(){
+    console.log(`price is ${this.score}`);
+    
+}
+
+const chai = new createUser("chai", 25);
+const tea =  new createUser("tea", 250);
+
+chai.printMe();
+
+/*
+
+Here-s what happens behind the scenes when the new keyword is used:
+
+A new keyword is created : the new keyword initiates the creation of a new Javascript object.
+
+*/
+
+
